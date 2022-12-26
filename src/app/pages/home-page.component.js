0f0e0341a715cad14af32054b4aecbd563d09b28
Julia Grib <yulgribgmail.com>
2022-12-26 12:02:07 +0300

    feat: add method for home-page

--- src/app/pages/home-page.component.js
+++ src/app/pages/home-page.component.js
@@ -4,9 +4,28 @@ class HomePageComponent extends WFMComponent {
     constructor(config){
         super(config)
     }
+
+    events() {
+        return {
+            'click .pluse': 'onPluseClick',
+        }
+    }
+
+    onPluseClick(event){
+        let num = document.querySelector('.num').innerHTML;
+        document.querySelector('.num').innerHTML = ++num;
+        
+
+    }
 }
 
 export const homePageComponent = new HomePageComponent({
     selector: 'app-home-page',
-    template: '<h1>Home Page</h1>'
+    template: `<h1>Home Page</h1>
+    <button class="pluse">+</button>
+    <p class="num">1</p>
+    
+    
+    
+    `
 })
--- src/framework/core/component.js
+++ src/framework/core/component.js
@@ -1,3 +1,5 @@
+import { wfm } from "../tools/util"
+
 export class Component {
     constructor(config){
         //шаблон
@@ -14,5 +16,24 @@ export class Component {
 
         //в созданный селектор вставляем шаблон
         this.el.innerHTML = this.template
+
+        this._initEvents()
     }
+
+    //не у всех компонентов есть методы events, 
+    //поэтому проверяем есть ли в компоненте метод events
+    _initEvents(){
+        if(wfm.isUdefined(this.events)) return
+
+        //если метод событий есть, то получаем объект событий
+        let events = this.events()
+
+        Object.keys(events).forEach(key => {
+            let listener = key.split(' ')
+            this.el
+            .querySelector(listener[1])
+            .addEventListener([listener[0]], this[events[key]].bind(this))
+        })
+    }
+
 }
