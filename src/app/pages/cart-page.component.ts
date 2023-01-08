4fb54ec84321106065abd99eb1772aacc63b186a
Viacheslav Bartosh <231globus@gmail.com>
2023-01-08 12:33:25 +0300

    refactor: cartpage to ts

--- src/app/pages/cart-page.component.ts
+++ src/app/pages/cart-page.component.ts
@@ -2,17 +2,19 @@ import { WFMComponent } from "../../framework/index";
 import { buyNow } from "./cart-page.utils/modal";
 import { paginator } from "./cart-page.utils/paginator"
 import { usedPromo } from "./cart-page.utils/used-promo"
-import { setCountOnceItem, setTempArrayProducts } from "./cart-page.utils/utils"
 import { headerCounter } from "../common/header.utils/counter"
+import { IConfigComponent, IProduct, IProductList } from "../../types";
 
 class CartPageComponent extends WFMComponent {
-  constructor(config){
+  constructor(config: IConfigComponent){
       super(config)
   }
   //глобальный объект продуктов в корзине
-  arrayProducts = {};
+  arrayProducts: IProductList  = {
+    products: []
+  };
   //глобальный счетчик кол-ва подуктов для каждого отдельного продукта в корзине
-  countOnceItem = [];
+  countOnceItem: (string | number)[] = [];
   //общая сумма 
   totalPriceN = 0;
   //новая цена после применения промокода
@@ -20,7 +22,7 @@ class CartPageComponent extends WFMComponent {
   //общее кол-во оваров
   totalProducts = 0;
   //массив неповторяющихся продуктов = arrayProducts.products
-  tempArrayProducts = [];
+  tempArrayProducts: Array<IProduct> = [];
 
   actions() {
     return {
@@ -36,12 +38,12 @@ class CartPageComponent extends WFMComponent {
   }
 
   makeCart() {
-    const productsCart = document.querySelector('.item-products__cart');
+    const productsCart = (document.querySelector('.item-products__cart')) as HTMLElement
     productsCart.innerHTML = ''
     this.arrayProducts = JSON.parse(localStorage.productsLocal);
     //проверяем не пустая ли корзина
     if(this.arrayProducts.products.length == 0) {
-      const container = document.querySelector('.container__cart')
+      const container = (document.querySelector('.container__cart')) as HTMLElement
       container.innerHTML = /*html*/`
       <div class="empty__cart">
         <h2>КОРЗИНА ПУСТА</h2>
@@ -51,28 +53,39 @@ class CartPageComponent extends WFMComponent {
       return
     }
     //создаем массив с колличеством каждого одинакого элемента 
-    setCountOnceItem(); 
+    // eslint-disable-next-line
+    this.countOnceItem = this.arrayProducts.products.reduce(function(acc: any, el: IProduct) {
+      acc[el.id] = (acc[el.id] || 0) + 1;
+      return acc;
+    }, {});
     //фильтруем повторяющиееся элементы 
-    setTempArrayProducts();
+    // eslint-disable-next-line
+    this.tempArrayProducts = this.arrayProducts.products.reduce((acc: any, el: IProduct) => {
+      // eslint-disable-next-line
+      if (!acc.find((v: any) => v.id == el.id)) {
+        acc.push(el);
+      }
+      return acc;
+    }, []); 
 
-    let params = new URLSearchParams(document.location.search);
+    const params = new URLSearchParams(document.location.search);
     
     if(params.get('buy')) {
       buyNow();
     }
 
-    const countProductsCart = document.querySelector('.count-items__cart');
-    const totalProductsCart = document.querySelector('.total-products__cart')
-    const totalPriceCart = document.querySelector('.total-price__cart')
+    const countProductsCart = (document.querySelector('.count-items__cart')) as HTMLElement
+    const totalProductsCart = (document.querySelector('.total-products__cart')) as HTMLElement
+    const totalPriceCart = (document.querySelector('.total-price__cart')) as HTMLElement
     countProductsCart.innerHTML = `Items: ${this.tempArrayProducts.length}`
     this.totalPriceN = 0;
     this.totalProducts = 0;
     //перебираем массива неповторяющихся продуктов и создаем из него DOM
     this.tempArrayProducts.forEach((elem, index) => {
-      let idProduct = elem.id
-      let priceProduct = elem.price
+      const idProduct = +elem.id
+      const priceProduct = +elem.price
       const itemCart = document.createElement('div');
-      const idItem = document.createElement('div');
+      const idItem = (document.createElement('div')) as HTMLElement
       const infoItem = document.createElement('div');
       const imgItem = document.createElement('img');
       const detailItem = document.createElement('div');
@@ -109,15 +122,15 @@ class CartPageComponent extends WFMComponent {
       addItem.setAttribute('data-id', `${elem.id}`);
       removeItem.setAttribute('data-id', `${elem.id}`);
 
-      idItem.innerHTML = index+1;
+      idItem.innerHTML = `${index+1}`;
       imgItem.src = elem.thumbnail;
       categoryItem.innerHTML = elem.category;
       nameItem.innerHTML = elem.title;
       descriptionItem.innerHTML = elem.description
-      countItem.innerHTML = this.countOnceItem[idProduct];
+      countItem.innerHTML = `${this.countOnceItem[idProduct]}`
       addItem.innerHTML = '+'
       removeItem.innerHTML = '-'
-      priceItem.innerHTML = `${priceProduct * this.countOnceItem[idProduct]}$`
+      priceItem.innerHTML = `${priceProduct * +this.countOnceItem[idProduct]}$`
       stockItem.innerHTML = `Stock: ${elem.stock}`
 
       productsCart.appendChild(itemCart);
@@ -136,9 +149,9 @@ class CartPageComponent extends WFMComponent {
       countItemContainer.appendChild(countItem)
       countItemContainer.appendChild(removeItem)
 
-      let totalItemPrice = elem.price * this.countOnceItem[idProduct]
+      const totalItemPrice = elem.price * +this.countOnceItem[idProduct]
       this.totalPriceN = this.totalPriceN + totalItemPrice
-      this.totalProducts = this.totalProducts + this.countOnceItem[idProduct]
+      this.totalProducts = this.totalProducts + +this.countOnceItem[idProduct]
       this.totalNewPrice =  this.totalPriceN
     })
     //выводи общее кол-во товаров и цену
@@ -154,29 +167,33 @@ class CartPageComponent extends WFMComponent {
 
     //добавляем кол-во товара
     addItem.forEach((elem) => {
-      elem.addEventListener('click', (event) => {
-        let currentId = event.target.dataset.id
-        let currentItem = this.arrayProducts.products.find(elem => elem.id == currentId);
+      // eslint-disable-next-line
+      elem.addEventListener('click', (event: any) => {
+        const target: HTMLElement = event.target
+        const currentId: number = +target.dataset.id
+        const currentItem = this.arrayProducts.products.find((elem) => elem.id == currentId);
         
         //проверяем что бы кол-во товара не было больше чем на складе
-        if(this.countOnceItem[currentId] === currentItem.stock) {
+        if((this.countOnceItem[currentId]) === currentItem.stock) {
           return
         }
         //пушим в локалсторадж
-        let localArr = JSON.parse( localStorage.productsLocal)
+        const localArr = JSON.parse( localStorage.productsLocal)
         localArr.products.push(currentItem);
         localStorage.productsLocal = JSON.stringify(localArr);
         headerCounter();
         ////////////////Обновляем цену и счетчикпродуктов в корзине//////////////
-        const countItem = document.querySelector(`.count-item-${currentId}__cart`);
-        const priceItem = document.querySelector(`.price-item-${currentId}__cart`);
-        const totalPriceCart = document.querySelector('.total-price__cart')
-        const totalProductsCart = document.querySelector('.total-products__cart')
-    
-        this.countOnceItem[currentId]++
+        const countItem = (document.querySelector(`.count-item-${currentId}__cart`)) as HTMLElement
+        const priceItem = (document.querySelector(`.price-item-${currentId}__cart`)) as HTMLElement
+        const totalPriceCart = (document.querySelector('.total-price__cart')) as HTMLElement
+        const totalProductsCart = (document.querySelector('.total-products__cart')) as HTMLElement
+        
+        let count = +this.countOnceItem[currentId]
+        count++
+        this.countOnceItem[currentId] = count
 
-        countItem.innerHTML = this.countOnceItem[currentId];
-        priceItem.innerHTML = `${currentItem.price * this.countOnceItem[currentId]}$`
+        countItem.innerHTML = `${this.countOnceItem[currentId]}`
+        priceItem.innerHTML = `${currentItem.price * +this.countOnceItem[currentId]}$`
         this.totalPriceN = this.totalPriceN + currentItem.price
         this.totalProducts++;
         totalPriceCart.innerHTML = `Total price: ${this.totalPriceN}$`
@@ -186,34 +203,37 @@ class CartPageComponent extends WFMComponent {
     })
     //удаляем товар
     removeItem.forEach((elem) => {
-      elem.addEventListener('click', (event) => {
-        let currentId = event.target.dataset.id
-        let currentItem = this.arrayProducts.products.find(elem => elem.id == currentId);
-        let indexItem = this.arrayProducts.products.indexOf(currentItem)
+      // eslint-disable-next-line
+      elem.addEventListener('click', (event: any) => {
+        const currentId = event.target.dataset.id
+        const currentItem = this.arrayProducts.products.find((elem: IProduct) => elem.id == currentId);
+        const indexItem = this.arrayProducts.products.indexOf(currentItem)
         //удаляем из локалстораджа
-        let localArr = JSON.parse( localStorage.productsLocal)
+        const localArr = JSON.parse( localStorage.productsLocal)
         localArr.products.splice(indexItem, 1);
         localStorage.productsLocal = JSON.stringify(localArr);
         headerCounter();
         //обновляем DOM
-        const countItem = document.querySelector(`.count-item-${currentId}__cart`);
-        const priceItem = document.querySelector(`.price-item-${currentId}__cart`);
-        const totalPriceCart = document.querySelector('.total-price__cart')
-        const totalProductsCart = document.querySelector('.total-products__cart')
+        const countItem = (document.querySelector(`.count-item-${currentId}__cart`)) as HTMLElement
+        const priceItem = (document.querySelector(`.price-item-${currentId}__cart`)) as HTMLElement
+        const totalPriceCart = (document.querySelector('.total-price__cart')) as HTMLElement
+        const totalProductsCart = (document.querySelector('.total-products__cart')) as HTMLElement
     
-        this.countOnceItem[currentId]--
+        let count = +this.countOnceItem[currentId]
+        count--
+        this.countOnceItem[currentId] = count
         //Проверяем если пустая карзина, то
         if(localArr.products.length == 0) {
-          const container = document.querySelector('.container__cart')
+          const container = (document.querySelector('.container__cart')) as HTMLElement
           container.innerHTML = /*html*/`
             <div class="empty__cart">
               <h2>КОРЗИНА ПУСТА</h2>
               <a class="main-link__cart" href="/">⟵ Вернуться на главную</a>
             </div>
           `
-          let url = new URL(window.location)
+          const url = new URL(window.location.href)
           url.searchParams.delete("page")
-          history.pushState(null, null, url);
+          history.pushState(null, '', url);
           return
         }
         
@@ -223,14 +243,14 @@ class CartPageComponent extends WFMComponent {
           if(localArr.products.length == currentPage * 3) {
             currentPage--;
             localStorage.currentPage = JSON.stringify(currentPage)
-            let url = new URL(window.location)
+            const url = new URL(window.location.href)
             url.searchParams.set('page', currentPage+1)
-            history.pushState(null, null, url);
+            history.pushState(null, '', url);
             this.makeCart();
           }
         }
-        countItem.innerHTML = this.countOnceItem[currentId];
-        priceItem.innerHTML = `${currentItem.price * this.countOnceItem[currentId]}$`
+        countItem.innerHTML = `${this.countOnceItem[currentId]}`
+        priceItem.innerHTML = `${currentItem.price * +this.countOnceItem[currentId]}$`
         this.totalPriceN = this.totalPriceN - currentItem.price
         totalPriceCart.innerHTML = `Total price: ${this.totalPriceN}$`
         this.totalProducts--;
@@ -239,17 +259,19 @@ class CartPageComponent extends WFMComponent {
       })
     })
     
-    const dropPromo = document.querySelectorAll('.remove-code__cart');
-    const applieidPromo = document.querySelector('.applied-promo__cart')
+    const dropPromo = (document.querySelectorAll('.remove-code__cart')) 
+    const applieidPromo = (document.querySelector('.applied-promo__cart')) as HTMLElement
 
     if(dropPromo) {
-      const totalPrice = document.querySelector('.total-price__cart')
-      const newPrice = document.querySelector('.new-price__cart')
+      const totalPrice = (document.querySelector('.total-price__cart'))as HTMLElement
+      const newPrice = (document.querySelector('.new-price__cart'))as HTMLElement
       dropPromo.forEach((elem) => {
-        elem.addEventListener('click', (event) => {
-          let arrayUsedPromo = JSON.parse(localStorage.usedPromo)
+        // eslint-disable-next-line
+        elem.addEventListener('click', (event: any) => {
+          const target: HTMLInputElement = event.target
+          const arrayUsedPromo = JSON.parse(localStorage.usedPromo)
           applieidPromo.remove();
-          arrayUsedPromo.splice(event.target.value, 1);
+          arrayUsedPromo.splice(target.value, 1);
           localStorage.usedPromo = JSON.stringify(arrayUsedPromo);
           this.totalNewPrice = this.totalNewPrice + Math.round(this.totalNewPrice * 0.1)
           newPrice.innerHTML = `New price: ${this.totalNewPrice }`
@@ -257,7 +279,7 @@ class CartPageComponent extends WFMComponent {
             localStorage.removeItem('usedPromo');
             totalPrice.classList.remove('line-through')
             newPrice.classList.remove('display-flex')
-            const appliedPromoContainer = document.querySelector('.applied-promo-container__cart')
+            const appliedPromoContainer = (document.querySelector('.applied-promo-container__cart')) as HTMLElement
             appliedPromoContainer.innerHTML = ''
             appliedPromoContainer.classList.remove('display-block')
           }
@@ -271,14 +293,16 @@ class CartPageComponent extends WFMComponent {
     buyNow();
   }
   
-  checkPromo(event) {
-    let value = event.target.value;
-    let usedPromoArray = []
+  checkPromo(event: InputEvent) {
+    // eslint-disable-next-line
+    const target: any = event.target
+    let value = target.value;
+    let usedPromoArray: string[] = []
     //проверяем если в локалсторадже есть промокоды, то забираем их
     if(localStorage.usedPromo) {
       usedPromoArray = JSON.parse(localStorage.usedPromo);
     }
-    let promoArray = ["rs", "epam"]
+    const promoArray = ["rs", "epam"]
 
     if (usedPromoArray.includes(value)) {
       return;
@@ -286,7 +310,7 @@ class CartPageComponent extends WFMComponent {
       
     if (promoArray.includes(value)) {
       usedPromoArray.push(value)
-      const succcesContainer = document.querySelector('.succes-promo-container__car')
+      const succcesContainer = (document.querySelector('.succes-promo-container__car')) as HTMLElement
       const succesPromo = document.createElement('div')
       const promoList = document.createElement('div')
       const addPromo = document.createElement('button')
@@ -312,48 +336,38 @@ class CartPageComponent extends WFMComponent {
   }
 }
 
-export const cartPageComponent = new CartPageComponent({
+export const cartPageComponent: CartPageComponent = new CartPageComponent({
     selector: 'app-cart-page',
     template: /*html*/`
     <div class="vertical__text">New Arrival</div>
       <div class="wrapper__main">
-          
+
           <div class="container__cart">
 
           <div class="products__cart">
             <div class="title-items__cart">
               <h2>Products in cart</h2>
               <div class="count-items__cart">Items: N</div>
-              <div class="pages__cart">
-            
-              </div>
+              <div class="pages__cart"></div>
             </div>
-            <div class="item-products__cart">
-              
-            </div>
+            <div class="item-products__cart"></div>
           </div>
           
           <div class="order__cart">
             <div class="title-order__cart">Summary</div>
             <div class="total-products__cart">Products: N</div>
             <div class="total-price__cart">Total price: N</div> 
-            <div class="applied-promo-container__cart">
-            </div>
+            <div class="applied-promo-container__cart"></div>
             <div class="new-price__cart">N</div> 
             <div class="test-promo__cart">Test promo: rs | epam</div>
-            <div class="succes-promo-container__car">
-
-            
-
-            </div>
+            <div class="succes-promo-container__car"></div>
             <div class="promo__cart">
               <input id="input-promo" class="input-promo__cart" type="search" minlength="6" maxlength="6" placeholder="Enter promo code"/>
             </div>
             <button class="buy-now-button__cart">Buy now</button>
-          </div>
 
+          </div>
         </div>
-      
       </div>
     `
 })
